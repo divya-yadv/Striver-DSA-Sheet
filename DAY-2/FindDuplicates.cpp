c93f359c3e389a5b70bd8d368845afb511fe263d
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-03 23:00:57 +0530

    add new method

--- DAY-2/FindDuplicates.cpp
+++ DAY-2/FindDuplicates.cpp
@@ -10,3 +10,18 @@ int findDuplicate(vector<int>& nums) {
         return -1;
     }
 // we can also use linked list cycle method to detect the cycle.
+int findDuplicate(vector < int > & nums) {
+  int slow = nums[0];
+  int fast = nums[0];
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow != fast);
+  fast = nums[0];
+  while (slow != fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+  return slow;
+}
+// both O(N), O(1) SOLUTIONS 
