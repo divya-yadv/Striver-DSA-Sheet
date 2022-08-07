cd177c43134583649424c7ad081c9be940e6a9c9
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-07 19:33:19 +0530

    Update IntersectionOfTwolinkedList.cpp

--- DAY-6/IntersectionOfTwolinkedList.cpp
+++ DAY-6/IntersectionOfTwolinkedList.cpp
@@ -1,1 +1,25 @@
 
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == headB)
+            return headA;
+        else if((headA == NULL) || (headB == NULL))
+            return NULL;
+        ListNode* A = headA;
+        ListNode* B = headB;
+        while(A || B)
+        {
+            if(A == B)
+                return A;
+          // here we will travserse two times max to reach intersection, when we initialize second time it covers rest of length of longer list and both become equal
+            else if(A == NULL)
+                A = headB;
+            else if(B == NULL)
+                B = headA;
+            else
+            {
+                A = A->next;
+                B = B->next;
+            }
+        }
+        return NULL;
+    }
