63e469dbfacbd35f8fd9dda2557f40c439b513ce
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-08 19:29:29 +0530

    Update RemoveDuplicates.cpp

--- DAY-7/RemoveDuplicates.cpp
+++ DAY-7/RemoveDuplicates.cpp
@@ -1,1 +1,13 @@
 
+int removeDuplicates(vector<int>& nums) {
+        int last = 0;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i] != nums[last])
+            {
+                last++;
+                nums[last] = nums[i];
+            }
+        }
+        return last+1;
+    }
