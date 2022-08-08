a10d925a49e3821effcfd3cb04e49b9c4b3b09d0
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-08 19:37:10 +0530

    Update MaxConsecutiveOnes.cpp

--- DAY-7/MaxConsecutiveOnes.cpp
+++ DAY-7/MaxConsecutiveOnes.cpp
@@ -2,29 +2,19 @@
 int findMaxConsecutiveOnes(vector<int>& nums) {
         int max_l = 0;
         int curr = 0;
-        if(nums[0] == 1)
-        {
-            max_l =1;
-            curr = 1;
-        }
-        for(int i=1;i<nums.size();i++)
+        for(int i=0;i<nums.size();i++)
         {
           // if consecutive
-            if((nums[i] == 1 )&& (nums[i] == nums[i-1]))
+            if(nums[i] == 1)
             {
                 curr++;
-                max_l = max(curr,max_l);
-            }
-          // if started
-            else if(nums[i] == 1)
-            {
-                curr = 1;
                 max_l = max(curr,max_l);
             }
             else
             {
                 curr = 0;
             }
+           
         }
         return max_l;
     }
