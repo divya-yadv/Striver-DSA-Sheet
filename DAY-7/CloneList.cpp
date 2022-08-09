728f16919ed4ee420a013d69bdffed79892d5592
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-09 18:23:12 +0530

    Update CloneList.cpp

--- DAY-7/CloneList.cpp
+++ DAY-7/CloneList.cpp
@@ -1,1 +1,65 @@
+// O(3*N) three traversal
+Node* copyRandomList(Node* head) {
+        if(head == NULL)
+        {
+            return NULL;
+        }
+        Node* temp = head;
+        // copy all nodes and make copied node the next of original node
+        while(temp)
+        {
+            Node* nextnode = new Node(temp->val);
+            nextnode->next = temp->next;
+            temp->next = nextnode;
+            temp = temp->next->next;
+        }
+        temp = head;
+        // point the random pointers for copied list
+        while(temp)
+        {
+            if(temp->random != NULL)
+                 temp->next->random = temp->random->next;
+            temp = temp->next->next;
+        }
+        // seperate both lists
+        Node* newhead = head->next;
+        Node* l2;
+        while(temp)
+        {
+            l2 = temp->next;
+            temp->next = l2->next;
+            if(l2->next != NULL)
+                l2->next = l2->next->next;
+            temp = temp->next;
+        }
+        return newhead;
+    }
 
+
+// O(N),O(N)
+Node* copyRandomList(Node* head) {
+        unordered_map<Node*,Node*>mp;
+        Node* temp = head;
+        Node* newlist = new Node(-1);
+        Node* x = newlist;
+        while(temp)
+        {
+            Node* newnode = new Node(temp->val);
+            newlist->next = newnode;
+            mp[temp] = newnode;
+            temp = temp->next;
+            newlist = newlist->next;
+        }
+        temp = head;
+        newlist = x->next;
+        while(temp)
+        {
+            if(temp->random != NULL)
+            {
+                newlist->random = mp[temp->random];
+            }
+            temp = temp->next;
+            newlist= newlist->next;
+        }
+        return x->next;
+    }
