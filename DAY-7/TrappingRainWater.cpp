ff937011d7e63f57a80a4f7a4f355e1f1cd848db
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-09 17:23:36 +0530

    Update TrappingRainWater.cpp

--- DAY-7/TrappingRainWater.cpp
+++ DAY-7/TrappingRainWater.cpp
@@ -1,1 +1,26 @@
+// O(N),O(N)
+ int trap(vector<int>& height) {
+        int n = height.size();
+        int larr[n];
+        int rarr[n];
+        larr[0] = height[0];
+        rarr[n-1] = height[n-1];
+        for(int i=1;i<n;i++)
+        {
+            larr[i] = max(larr[i-1],height[i]);
+        }
+         for(int i=n-2;i>=0;i--)
+        {
+            rarr[i] = max(rarr[i+1],height[i]);
+        }
+        int ans = 0;
+        for(int i=1;i<n-1;i++)
+        {
+            ans += min(larr[i],rarr[i])-height[i];
+        }
+        return ans; 
+    }
+
+
+// O(N),O(1)
 
