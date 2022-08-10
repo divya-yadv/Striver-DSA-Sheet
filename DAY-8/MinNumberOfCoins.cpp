4a089145fadc1475feda3698b1234d5ee7011ef3
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-10 19:40:32 +0530

    Update MinNumberOfCoins.cpp

--- DAY-8/MinNumberOfCoins.cpp
+++ DAY-8/MinNumberOfCoins.cpp
@@ -1,1 +1,18 @@
 
+int minCoins(int coins[], int M, int V) 
+	{ 
+	    
+	   vector<int>dp(V+1,INT_MAX);
+	   dp[0] = 0;
+	   for(int i=1;i<=V;i++)
+	   {
+	       for(int j=0;j<M;j++)
+	       {
+	           if(coins[j] <= i && dp[i-coins[j]] != INT_MAX)
+	           {
+	               dp[i] = min(dp[i],1+dp[i-coins[j]]);
+	           }
+	       }
+	   }
+	   return dp[V] ==INT_MAX ?-1 : dp[V];
+	}
