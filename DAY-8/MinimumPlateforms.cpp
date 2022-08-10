b16eb696e882f669d1f61897cc35ed531d7fe7f5
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-10 13:18:24 +0530

    Update MinimumPlateforms.cpp

--- DAY-8/MinimumPlateforms.cpp
+++ DAY-8/MinimumPlateforms.cpp
@@ -22,3 +22,30 @@ int findPlatform(int arr[], int dep[], int n)
         }
         return ans;
     }
+
+// check for the intervals that overlap the current one 
+int findPlatform(int arr[], int dep[], int n)
+{
+ 
+    // plat_needed indicates number of platforms
+    // needed at a time
+    int plat_needed = 1, result = 1;
+    int i = 1, j = 0;
+ 
+    // run a nested  loop to find overlap
+    for (int i = 0; i < n; i++) {
+        // minimum platform
+        plat_needed = 1;
+ 
+        for (int j = i + 1; j < n; j++) {
+            // check for overlap
+            if (max(arr[i], arr[j]) <= min(dep[i], dep[j]))
+                plat_needed++;
+        }
+ 
+        // update result
+        result = max(result, plat_needed);
+    }
+ 
+    return result;
+}
