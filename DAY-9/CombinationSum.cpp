167b89fce0412bfd94e61416c734e164eeacdab1
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-12 12:48:30 +0530

    Create CombinationSum.cpp

---
+++ DAY-9/CombinationSum.cpp
@@ -0,0 +1,26 @@
+void solve(vector<int> candidates,int target, int i,vector<int>curr,vector<vector<int>>&res)
+    {
+        if(i == candidates.size())
+        {
+            return;
+        }
+        if(target == 0)
+        {
+            res.push_back(curr);
+            return;
+        }
+  // consider two cases added or not added
+        curr.push_back(candidates[i]);
+        if(target >= candidates[i])
+        {
+            solve(candidates,target-candidates[i],i,curr,res);
+        }
+        curr.pop_back();
+        solve(candidates,target,i+1,curr,res);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> curr;
+        solve(candidates,target,0,curr,res);
+        return res;
+    }
