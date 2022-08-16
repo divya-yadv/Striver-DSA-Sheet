e1ba232a4d50d1f301daafc9f0c178098ff1a937
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-16 22:26:29 +0530

    recursive solution

--- DAY-9/PalindromePartitioning.cpp
+++ DAY-9/PalindromePartitioning.cpp
@@ -1,1 +1,42 @@
+// recursive solution
+// O(2^n*k*(n/2)) -> 2^n for all combinations , O(n/2) to check if its palindrome , O(K) is to insert it into a vector
+// O(k*x)
 
+
+bool isPalindrome(string s,int start,int end)
+    {
+        while(start<=end)
+        {
+            if(s[start++] != s[end--])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    void solve(string s,int i,vector<string>&v,vector<vector<string>>&res)
+    {
+      // if travsersed whole string then push the result 
+        if(i == s.length())
+        {
+            res.push_back(v);
+            return;
+        }
+      // start making palindrome for particular point 
+        for(int j=i;j<s.length();j++)
+        {
+          // check of every i,j
+            if(isPalindrome(s,i,j))
+            {
+                v.push_back(s.substr(i,j-i+1));
+                solve(s,j+1,v,res);
+                v.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res;
+        vector<string> v;
+        solve(s,0,v,res);
+        return res;
+    }
