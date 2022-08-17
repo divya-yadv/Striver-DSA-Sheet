9965b581f87b14a062af53eeef8a90d8d0c2af13
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-17 07:11:22 +0530

    Update AllPermutations.cpp

--- DAY-10/AllPermutations.cpp
+++ DAY-10/AllPermutations.cpp
@@ -1,1 +1,22 @@
-
+// O(N!*N) -> N to push this into vector 
+// O(N!)
+void solve(vector<int>&nums,vector<vector<int>>&res,int index)
+    {
+        if(index == nums.size())
+        {
+            res.push_back(nums);
+            return;
+        }
+  // fix for index and swap with every possible value
+        for(int i=index;i<nums.size();i++)
+        {
+            swap(nums[i],nums[index]);
+            solve(nums,res,index+1);
+            swap(nums[i],nums[index]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>res;
+        solve(nums,res,0);
+        return res;
+    }
