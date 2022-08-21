4181e1296cbd8fd3160ef7358aff8c84aa7dc761
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-21 15:35:05 +0530

    Update Maximum_sum_combination.cpp

--- DAY-12/Maximum_sum_combination.cpp
+++ DAY-12/Maximum_sum_combination.cpp
@@ -1,54 +1,73 @@
+// Time complexity : O(nlogn)
 vector<int> Solution::solve(vector<int> &A, vector<int> &B, int C) {
-    // initialise minheap
-    int N = A.size();
-    sort(A.begin(),A.end());
-    sort(B.begin(),B.end());
-     priority_queue<pair<int, pair<int, int> > > pq;
-      set<pair<int, int> > my_set;
-    pq.push(make_pair(A[N - 1] + B[N - 1],
-                      make_pair(N - 1, N - 1)));
- 
-    my_set.insert(make_pair(N - 1, N - 1));
-    vector<int> ans;
-    for(int k=0;k<C;k++)
-    {
-         pair<int, pair<int, int> > temp = pq.top();
-        pq.pop();
- 
-        ans.push_back(temp.first);
- 
-        int i = temp.second.first;
-        int j = temp.second.second;
- 
-        int sum = A[i - 1] + B[j];
- 
-        // insert (A[i - 1] + B[j], (i - 1, j))
-        // into max heap.
-        pair<int, int> temp1 = make_pair(i - 1, j);
- 
-        // insert only if the pair (i - 1, j) is
-        // not already present inside the map i.e.
-        // no repeating pair should be present inside
-        // the heap.
-        if (my_set.find(temp1) == my_set.end())
-        {
-            pq.push(make_pair(sum, temp1));
-            my_set.insert(temp1);
-        }
- 
-        // insert (A[i] + B[j - 1], (i, j - 1))
-        // into max heap.
-        sum = A[i] + B[j - 1];
-        temp1 = make_pair(i, j - 1);
- 
-        // insert only if the pair (i, j - 1)
-        // is not present inside the heap.
-        if (my_set.find(temp1) == my_set.end())
-        {
-            pq.push(make_pair(sum, temp1));
-            my_set.insert(temp1);
-        }
-    }
+
     
+
+    int n=A.size();
+
+    vector<int>ans;
+
+    // sort both arrays 
+
+    sort(A.begin(), A.end());
+
+    sort(B.begin(), B.end());
+
+    // Store sum -> (i,j)
+
+    priority_queue<pair<int, pair<int, int>>>pq; // (sum, (i,j))
+
+    set<pair<int,int>>st;
+
+    
+
+    pq.push({A[n-1]+B[n-1],{n-1,n-1}});
+
+    st.insert({n-1,n-1}); // which pair of indices we have used already, to keep a track of them
+
+    
+
+    while(C--)
+
+    {
+
+        auto p = pq.top();pq.pop();
+
+        int sum = p.first; 
+
+        int i = p.second.first;
+
+        int j = p.second.second;
+
+        // max sum -> answer
+
+        ans.push_back(sum);
+
+        
+
+        // Next two sum combinations (i-1,j), (i,j-1)
+
+        sum = A[i-1] + B[j];
+
+        if(st.find({i-1,j})==st.end()){
+
+            pq.push({sum, {i-1,j}});
+
+            st.insert({i-1,j});
+
+        }
+
+        sum = A[i] + B[j-1];
+
+        if(st.find({i,j-1})==st.end()){
+
+            pq.push({sum, {i,j-1}});
+
+            st.insert({i,j-1});
+
+        }
+
+    }
+
     return ans;
 }
