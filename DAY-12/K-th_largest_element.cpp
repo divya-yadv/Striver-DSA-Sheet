999fe8c09d96682bd11c0e0b4421033a26b3f26c
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-21 15:11:34 +0530

    Update K-th_largest_element.cpp

--- DAY-12/K-th_largest_element.cpp
+++ DAY-12/K-th_largest_element.cpp
@@ -16,3 +16,35 @@
         return ans;
     }
 /// we can use quicksort also in order to find kth largest element as we find position of pivot
+int partition(vector<int>& nums,int left,int right){
+	int pivot = nums[left], l=left+1, r = right;
+	while(l<=r){
+		if(nums[l]<pivot && nums[r]>pivot) swap(nums[l++],nums[r--]);
+		if(nums[l]>=pivot) ++l;
+		if(nums[r]<=pivot) --r;
+	}
+	swap(nums[left], nums[r]);
+	return r;
+}
+    int findKthLargest(vector<int>& nums, int k) {
+        int l = 0;
+        int r = nums.size()-1;
+        int p=0;
+        while(1)
+        {
+            p = partition(nums,l,r);
+            if(p == (k-1))
+            {
+                break;
+            }
+             else if(p<k-1)
+            {
+                l = p+1;
+            }
+           else
+           {
+               r = p-1;
+           }
+        }
+        return nums[p];
+    }
