c075ded0596eb72dc9cded78eef874836d57421d
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-21 14:50:21 +0530

    Update K-th_largest_element.cpp

--- DAY-12/K-th_largest_element.cpp
+++ DAY-12/K-th_largest_element.cpp
@@ -1,1 +1,18 @@
-
+// O(N*logN)
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int> , greater<int>> minHeap;
+        int i=0;
+        while(i<nums.size())
+        {
+            minHeap.push(nums[i]);
+            int n = minHeap.size();
+            if(n > k)
+            {
+                minHeap.pop();
+            }
+            i++;
+        }
+        int ans = minHeap.top();
+        return ans;
+    }
+/// we can use quicksort also in order to find kth largest element as we find position of pivot
