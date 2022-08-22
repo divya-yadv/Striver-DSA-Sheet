82533bba70f674e94c6e80698567d08ce853964d
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-22 19:14:36 +0530

    Update ImplementQueueUsingStacks.cpp

--- DAY-13/ImplementQueueUsingStacks.cpp
+++ DAY-13/ImplementQueueUsingStacks.cpp
@@ -11,7 +11,7 @@ public:
         st1.push(x);
         size++;
     }
-    
+    // O(N)
     int pop() {
         if(size == 0)
         {
@@ -32,7 +32,7 @@ public:
         size--;
         return ans;
     }
-    
+    // O(N)
     int peek() {
         if(size == 0)
         {
@@ -50,6 +50,73 @@ public:
             st2.pop();
         }
         return ans;
+    }
+    
+    bool empty() {
+        if(size == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+
+class MyQueue {
+public:
+    stack<int>st1;
+    stack<int>st2;
+    int size;
+    MyQueue() {
+        size=0;
+    }
+    
+    void push(int x) {
+        st1.push(x);
+        size++;
+    }
+    
+    int pop() {
+        if(size == 0)
+        {
+            return -1;
+        }
+        if(st2.empty())
+        {
+            while(!st1.empty())
+            {
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+        int x = st2.top();
+        st2.pop();
+        size--;
+        return x;
+    }
+    
+    int peek() {
+        if(size == 0)
+        {
+            return -1;
+        }
+        if(size == 0)
+        {
+            return -1;
+        }
+        if(st2.empty())
+        {
+            while(!st1.empty())
+            {
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+        int x = st2.top();
+        return x;
     }
     
     bool empty() {
