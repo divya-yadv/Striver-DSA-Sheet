c340fd4be382a1f96a0acc028c978b5ad82f2840
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-22 20:52:28 +0530

    Update SortStack.cpp

--- DAY-13/SortStack.cpp
+++ DAY-13/SortStack.cpp
@@ -1,1 +1,25 @@
 
+#include<bits/stdc++.h>
+void sortInsert(stack<int>&stack,int num)
+{
+    if((stack.empty()) || ((!stack.empty())&& (stack.top()<num)))
+    {
+        stack.push(num);
+        return;
+    }
+    int n = stack.top();
+    stack.pop();
+  // again find the position for num
+    sortInsert(stack,num);
+    stack.push(n);
+}
+void sortStack(stack<int> &stack)
+{
+	if(stack.empty())
+        return;
+    int num = stack.top();
+    stack.pop();
+    
+    sortStack(stack);
+    sortInsert(stack,num);
+}
