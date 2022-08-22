d0d92f2eaa272298394cca7965aed5cec39e716a
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-22 19:40:39 +0530

    Update NextGreaterElement.cpp

--- DAY-13/NextGreaterElement.cpp
+++ DAY-13/NextGreaterElement.cpp
@@ -31,3 +31,26 @@ vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         }
         return ans;
     }
+// O(N+M)
+ vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans(nums1.size(),-1);
+        unordered_map<int,int>mp;
+        stack<int>st;
+         // pre-compute next greater element and store it in map
+        for(auto n:nums2)
+        {
+            while(!st.empty() && (st.top()<n))
+            {
+                mp[st.top()] = n;
+                st.pop();
+            }
+            st.push(n);
+        }
+        for (int i=0; i<nums1.size(); i++) {
+        auto itr = mp.find(nums1[i]);
+        if (itr != mp.end()) {
+            ans[i] = itr->second;
+        }
+    }
+        return ans;
+    }
