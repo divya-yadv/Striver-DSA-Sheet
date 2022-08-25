65a9030d1410688deab31bd6a8d90d06757ae228
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-25 19:55:19 +0530

    Update LRU_cache.cpp

--- DAY-14/LRU_cache.cpp
+++ DAY-14/LRU_cache.cpp
@@ -1,4 +1,11 @@
-//
+/* While inserting the {key,val} pair into the DDL we need to make sure that we are inserting it from the back tail to the head.
+// The cache will tell us when the {key, value} pair is used/inserted.
+
+// So, while inserting the {key,val}pair, we need to:
+➡ Check if the {key,val} pair is already present in Cache or not.
+➡ Check for the capacity, if the (Cache size == capacity) then while inserting the new pair we need to remove the LRU and insert the new pair right after the head & while removing the node we need to make sure that we remove the {value, node} pair from the cache.
+➡Finally check, If the key is not present in the Cache then return -1; */
+
 class LRUCache {
 public:
     list<int>l;
@@ -40,5 +47,78 @@ public:
             }
         
         }
+    }
+};
+
+// using double linked list O(1)
+class LRUCache {
+public:
+    class Node {
+        public:
+        int key;
+        int val;
+        Node* prev;
+        Node* next;
+        Node(int _key,int _val)
+        {
+            key = _key;
+            val = _val;
+            prev = NULL;
+            next = NULL;
+        } 
+    };
+    unordered_map<int,Node* > mp;
+    Node* head=new Node(-1,-1);
+    Node* tail = new Node(-1,-1);
+    int cap;
+    LRUCache(int capacity) {
+        cap = capacity;
+        head->next = tail;
+        tail ->prev = head;
+    }
+    void addnode(Node* temp)
+    {
+        Node* nextptr = head->next;
+        temp->next = nextptr;
+        nextptr->prev = temp;
+        head->next = temp;
+        temp->prev =  head;
+    }
+    void deletenode(Node* temp)
+    {
+       Node* nextptr = temp->next;
+        Node* prevptr = temp->prev;
+        nextptr->prev = prevptr;
+        prevptr->next = nextptr;
+    }
+    int get(int key) {
+        if(mp.find(key) == mp.end())
+        {
+            return -1;
+        }
+        Node* x = mp[key];
+        deletenode(x);
+        addnode(x);
+        mp[key] = head->next;
+        return x->val;
+    }
+    
+    void put(int key, int value) {
+       
+        if(mp.find(key) != mp.end())
+        {
+             deletenode(mp[key]);
+            mp.erase(key);
+           
+        }
+         Node* temp =new Node(key,value);
+        addnode(temp);
+        if(mp.size() == cap)
+        {
+            mp.erase(tail->prev->key);
+            deletenode(tail->prev);
+         
+        }
+        mp[key] = head->next;
     }
 };
