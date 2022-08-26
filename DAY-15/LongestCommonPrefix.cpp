add0c4a6c0a7eea48f3c95dbce437601cbf2a725
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-26 18:24:39 +0530

    Update LongestCommonPrefix.cpp

--- DAY-15/LongestCommonPrefix.cpp
+++ DAY-15/LongestCommonPrefix.cpp
@@ -28,3 +28,30 @@ string longestCommonPrefix(vector<string>& strs) {
         return s;
         
     }
+
+ string longestCommonPrefix(vector<string>& strs) {
+        // there is one more way to do this we can find the lexigraphical order of first and last string these are the strings that will show maximum difference 
+        string start = strs[0];
+        string end = strs[0];
+        for(int i=1;i<strs.size();i++)
+        {
+            if(strs[i]< start)
+            {
+                start = strs[i];
+            }
+            if(strs[i]> end)
+            {
+                end = strs[i];
+               
+            }
+        }
+        string ans="";
+        for(int i=0;i<min(start.size(),end.size());i++)
+        {
+            if(start[i] == end[i])
+               ans += start[i];
+            else
+                return ans;
+        }
+        return ans;
+    }
