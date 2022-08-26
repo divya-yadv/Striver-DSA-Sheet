25f6b1d2a72870051b4b656b1ca32cf63a1a452e
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-26 13:48:21 +0530

    Update reverse_words_in_string.cpp

--- DAY-15/reverse_words_in_string.cpp
+++ DAY-15/reverse_words_in_string.cpp
@@ -26,3 +26,29 @@ string reverseWords(string s) {
         res.pop_back();
         return res;
     }
+
+// O(N),O(1)
+
+string reverseWords(string s) {
+        string curr="";
+         string res="";
+        // traverse from last and store a word in reverse order
+        // add it to res
+        for(int i=s.size()-1;i>=0;i--)
+        {
+            if(s[i] != ' ')
+            {
+                curr = s[i]+curr;
+            }
+            else
+            {
+                if(curr != "")
+                    res += curr + ' ';
+                curr="";
+            }
+        }
+        if(curr != "")
+             res += curr + ' ';
+        res.pop_back();
+        return res;
+    }
