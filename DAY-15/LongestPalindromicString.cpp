c7848d3037e665cb190e3044ea09d713572cd34d
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-26 19:04:09 +0530

    Create LongestPalindromicString.cpp

---
+++ DAY-15/LongestPalindromicString.cpp
@@ -0,0 +1,33 @@
+//O(N^2)
+// check for every substring
+bool isPalindrome(string s,int i,int j)
+    {
+        while(i<=j)
+        {
+            if(s[i] != s[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    string longestPalindrome(string s) {
+        int res = 0;
+        string p="";
+        for(int i=0;i<s.size();i++)
+        {
+            for(int j=i;j<s.size();j++)
+            {
+                if(isPalindrome(s,i,j)){
+                    if(j-i+1 > res)
+                    {
+                        res = max(res,j-i+1);
+                        p = s.substr(i,j-i+1);
+                    }
+                }
+            }
+        }
+        return p;
+    }
