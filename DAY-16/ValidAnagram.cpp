ca971bf915bd59b051df5628b880f24eb49f0907
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-27 10:54:59 +0530

    Create ValidAnagram.cpp

---
+++ DAY-16/ValidAnagram.cpp
@@ -0,0 +1,16 @@
+ bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+            return false;
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.length();i++)
+        {
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+      for(auto x:mp)
+      {
+          if(x.second != 0)
+              return false;
+      }
+    return true;
+    }
