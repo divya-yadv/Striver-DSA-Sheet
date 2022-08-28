4d640c48c766a36e5d1b76a86447e7a2460a90b9
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-28 12:09:51 +0530

    Create postorder.cpp

---
+++ DAY-17/postorder.cpp
@@ -0,0 +1,14 @@
+// recursive 
+void postorder(TreeNode* root,vector<int>&res)
+    {
+        if(root == NULL)
+            return;
+        postorder(root->left,res);
+        postorder(root->right,res);
+        res.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>res;
+        postorder(root,res);
+        return res;
+    }
