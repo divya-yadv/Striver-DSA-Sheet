7e663edddec3d91e771abcee1efd2b90e0505ec2
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-28 13:43:32 +0530

    Update LeftViewofBinaryTree.cpp

--- DAY-17/LeftViewofBinaryTree.cpp
+++ DAY-17/LeftViewofBinaryTree.cpp
@@ -1,4 +1,7 @@
+
+// FOR LEFT VIEW
 // do level order traversal and get the first element at each level
+
 vector<int> leftView(Node *root)
 {
     vector<int>res;
@@ -9,6 +12,7 @@ vector<int> leftView(Node *root)
     while(!q.empty())
     {
         int count = q.size();
+        // will push first element
         res.push_back(q.front()->data);
         while(count--)
         {
@@ -38,6 +42,57 @@ void recursion(Node *root, int level, vector<int> &res)
         recursion(root->right, level+1, res);
 }
 vector<int> leftView(Node *root)
+{
+        vector<int> res;
+        recursion(root, 0, res);
+        return res;
+}
+
+// FOR RIGHT VIEW
+
+vector<int> rightView(Node *root)
+{
+    vector<int>res;
+    if(root == NULL)
+        return res;
+    queue<Node*>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        int count = q.size();
+        
+        while(count--)
+        {
+            // will push last one
+            if(count == 1)
+                 res.push_back(q.front()->data);
+            Node* temp = q.front();
+            q.pop();
+            if(temp->left)
+            {
+                q.push(temp->left);
+            }
+            if(temp ->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+    return res;
+}
+
+// recursive 
+// O(N), o(h)
+void recursion(Node *root, int level, vector<int> &res)
+{
+        if(root==NULL) return ;
+    // if right exist it will come first and as we pushed right res size will change and if right doesn't exist res size will be same for left and we can push it
+        if(res.size()==level) res.push_back(root->data);
+        
+        recursion(root->right, level+1, res);
+        recursion(root->left, level+1, res);
+}
+vector<int> rightView(Node *root)
 {
         vector<int> res;
         recursion(root, 0, res);
