4d278893606a80560bcc5ef660e55df1e26f2a67
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-28 13:24:52 +0530

    Update postorder.cpp

--- DAY-17/postorder.cpp
+++ DAY-17/postorder.cpp
@@ -12,3 +12,56 @@ void postorder(TreeNode* root,vector<int>&res)
         postorder(root,res);
         return res;
     }
+
+// itervative with two stacks
+vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>res;
+        stack<TreeNode*>st1;
+        stack<int>st2;
+        if(root == NULL)
+            return res;
+        st1.push(root);
+        while(!st1.empty())
+        {
+            TreeNode* temp = st1.top();
+            st1.pop();
+            st2.push(temp->val);
+            if(temp->left)
+               st1.push(temp->left);
+            if(temp->right)
+               st1.push(temp->right);
+        }
+        while(!st2.empty())
+        {
+            res.push_back(st2.top());
+            st2.pop();
+        }
+        return res;
+    }
+
+// iterative with one stack
+vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>res;
+        stack<TreeNode*>st1;
+        if(root == NULL)
+            return res;
+        st1.push(root);
+        while(!st1.empty())
+        {
+            TreeNode* temp = st1.top();
+            st1.pop();
+            res.push_back(temp->val);
+            if(temp->left)
+               st1.push(temp->left);
+            if(temp->right)
+               st1.push(temp->right);
+        }
+    // we stored in reverse order
+      reverse(res.begin(),res.end());
+        return res;
+    }
+
+
+// one more iterative
+
+//
