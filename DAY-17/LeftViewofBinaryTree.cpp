6c67cb311780ede006c1ad1f5ba1d35b2f3bb2c3
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-28 13:40:28 +0530

    Update LeftViewofBinaryTree.cpp

--- DAY-17/LeftViewofBinaryTree.cpp
+++ DAY-17/LeftViewofBinaryTree.cpp
@@ -26,3 +26,20 @@ vector<int> leftView(Node *root)
     }
     return res;
 }
+
+// recursive 
+// O(N), o(h)
+void recursion(Node *root, int level, vector<int> &res)
+{
+        if(root==NULL) return ;
+    // if left exist it will come first and as we pushed left res size will change and if left doesn't exist res size will be same for right and we can push it
+        if(res.size()==level) res.push_back(root->data);
+        recursion(root->left, level+1, res);
+        recursion(root->right, level+1, res);
+}
+vector<int> leftView(Node *root)
+{
+        vector<int> res;
+        recursion(root, 0, res);
+        return res;
+}
