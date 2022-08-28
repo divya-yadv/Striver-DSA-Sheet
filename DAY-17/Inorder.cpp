23bc7989065f12c8118ed65654bfaf0c612175ef
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-08-28 11:23:06 +0530

    Update Inorder.cpp

--- DAY-17/Inorder.cpp
+++ DAY-17/Inorder.cpp
@@ -1,1 +1,14 @@
-
+// recursive solution
+ void inorder(TreeNode* root,vector<int>&res)
+    {
+        if(root == NULL)
+            return;
+        inorder(root->left,res);
+        res.push_back(root->val);
+        inorder(root->right,res);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        inorder(root,res);
+        return res;
+    }
